577bdba3f3b62ff5c88a3bb67019787d49b3366f
Andy Braga <ab@luckypike.com>
2017-06-14 12:10:18 +0300

    Оформление каталога

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,7 +22,7 @@ class ProductsController < ApplicationController
     @product = Product.new(product_params)
 
     if @product.save
-      redirect_to [@product.category, @product], notice: 'Product was successfully created.'
+      redirect_to @product, notice: 'Product was successfully created.'
     else
       render :new
     end
@@ -30,7 +30,7 @@ class ProductsController < ApplicationController
 
   def update
     if @product.update(product_params)
-      redirect_to [@product.category, @product], notice: 'Product was successfully updated.'
+      redirect_to @product, notice: 'Product was successfully updated.'
     else
       render :edit
     end
