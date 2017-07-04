055f3606220f16736039f03df4a5541b2748e5db
Andy Braga <ab@luckypike.com>
2017-07-04 19:01:54 +0300

    Вывод меню и отбивка и кайндов

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 module ApplicationHelper
+  def menu_catalog?
+    p %w(themes products).include? controller_name
+  end
 
   def typekit_include_tag
     javascript_include_tag('//use.typekit.net/crp6yvg.js') +
