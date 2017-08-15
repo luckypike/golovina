feeaea1f5c62668cbe9791a3f57b37b21d0d7d7b
Andy Braga <ab@luckypike.com>
2017-08-15 11:35:54 +0300

    Небольшие правки оформления

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170814160629) do
+ActiveRecord::Schema.define(version: 20170815081345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,8 @@ ActiveRecord::Schema.define(version: 20170814160629) do
     t.bigint "kind_id"
     t.integer "state", default: 0
     t.string "title"
+    t.boolean "sale", default: false
+    t.boolean "new", default: false
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["colors"], name: "index_products_on_colors", using: :gin
     t.index ["kind_id"], name: "index_products_on_kind_id"
