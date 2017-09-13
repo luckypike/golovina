8d1f441fe5f4fdedbbb211e41fbcbdd2531a8e8e
Andy Braga <ab@luckypike.com>
2017-09-13 12:25:10 +0300

    Опционален родитель у категории

--- app/models/category.rb
+++ app/models/category.rb
@@ -10,7 +10,7 @@ class Category < ApplicationRecord
 
   has_many :products
 
-  belongs_to :parent_category, class_name: 'Category'
+  belongs_to :parent_category, class_name: 'Category', optional: true
   has_many :categories, class_name: "Category", foreign_key: "parent_category_id"
 
   def normalize_friendly_id(input)
