e59bc5e19050961a00b69331aecffb1fb3eea00f
Andy Braga <ab@luckypike.com>
2017-09-15 19:59:20 +0300

    Хост на продакшне

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,8 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.default_url_options = { host: Rails.application.secrets[:host] }
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.yandex.ru',
