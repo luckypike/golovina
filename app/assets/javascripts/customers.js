e270b40e3c5675a07c2b4d384f65733a8d6cd79d
Sidlike <almostkolyan@gmail.com>
2017-10-17 19:18:46 +0300

    Изменен текст на странице 'Возврат и обмен'. На стрнице 'Оплата и доставка' изменен текст и добавлены раскрывающиеся плашки городов.

--- app/assets/javascripts/customers.js
+++ app/assets/javascripts/customers.js
@@ -1,2 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function() {
+  $('.city_toggle .lv4').on('click', function() {
+    $('.city_toggle .content').not($(this).next()).slideUp();
+    $(this).next().slideToggle();
+  });
+});
