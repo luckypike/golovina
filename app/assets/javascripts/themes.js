fd233170b7a7c6cfd54196ac1f4f4f5885c7fe0d
Sidlike <almostkolyan@gmail.com>
2017-12-01 16:26:59 +0300

    Изменено отображение страницы "Все настроения"

--- app/assets/javascripts/themes.js
+++ app/assets/javascripts/themes.js
@@ -9,4 +9,36 @@ $(function() {
       }
     }).trigger('scroll');
   }
+
+  $('.theme_intro_wrapper').on('click', function(e){
+    if(!$(e.target).hasClass('edit')) {
+      var _this = $(this);
+      var _tkw = $(this).next();
+      var _tk = _tkw.find('.theme_kits');
+
+
+      if(_tkw.is(':visible')) {
+        _tkw.slideUp(400, function() {
+          var swiper = _tk.data('swiper');
+
+          if(swiper) {
+            swiper.destroy(true, true);
+          }
+        });
+
+      } else {
+        $("html, body").stop().animate({ scrollTop: (_this.offset().top + _this.height() / 2) }, 500, 'swing');
+
+        _tkw.slideToggle(400, function() {
+          if($(window).width() < 1280) {
+            var swiper = new Swiper (_tk, {
+              freeMode: false,
+              slidesPerView: 'auto',
+            });
+            _tk.data('swiper', swiper);
+          }
+        });
+      }
+    }
+  });
 });
