7194b31d853473d59fd3f08cd8ad29da2663912b
Sidlike <almostkolyan@gmail.com>
2017-12-07 19:32:24 +0300

    Доработки страницы about/golovina

--- app/assets/javascripts/about.js
+++ app/assets/javascripts/about.js
@@ -5,11 +5,61 @@ $(function(){
     swiper_create(_as);
   }
 
-  if($('.collection_slider').length > 0) {
-    _as = $('.swiper-container');
+  console.log('asd');
+  var _window = $(window);
+  var _pacw = $('.page_about_collection_wrapper');
 
-    swiper_create(_as);
+  if(_pacw.length > 0) {
+    _window.on('scroll', function() {
+      $('.header').toggleClass('about_act', _pacw.height() - 30 <= _window.scrollTop());
+    }).trigger('scroll');
   }
+
+  if($('.collection_images').length > 0) {
+    _as = $('.swiper-container-image');
+
+    var col_image = new Swiper (_as, {
+      freeMode: false,
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      speed: 600,
+      pagination: $('.swiper-pagination', _as),
+      paginationType: 'bullets',
+      paginationFractionRender: function(swiper, currentClassName, totalClassName) {
+        return '<span class="' + currentClassName + '"></span>' +
+                '/' +
+                '<span class="' + totalClassName + '"></span>';
+      },
+      paginationClickable: true,
+    });
+
+    // swiper_create(_as);
+  }
+
+  if($('.collection_thumbs').length > 0) {
+    _as = $('.swiper-container-thumbs');
+
+    _as.append('<div class="swiper-pagination"></div>');
+    var col_thumbs = new Swiper (_as, {
+      freeMode: false,
+      slidesPerView: 'auto',
+      centeredSlides: true,
+      speed: 600,
+      pagination: $('.swiper-pagination', _as),
+      paginationType: 'bullets',
+      paginationFractionRender: function(swiper, currentClassName, totalClassName) {
+        return '<span class="' + currentClassName + '"></span>' +
+                '/' +
+                '<span class="' + totalClassName + '"></span>';
+      },
+      paginationClickable: true,
+    });
+
+    // swiper_create(_as);
+  }
+
+  col_image.params.control = col_thumbs;
+  col_thumbs.params.control = col_image;
 
   function swiper_create(swpr_cntnr) {
     _as.append('<div class="swiper-pagination"></div>');
