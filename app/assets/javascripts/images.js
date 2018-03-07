c64992b68dfc0f6cba3befae89555579fcbdaf3c
Andy Braga <ab@luckypike.com>
2018-03-07 17:00:23 +0300

    Увеличение фоток

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -1,2 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function(){
+  $('.product_images').on('click', function() {
+    console.log('QQQ');
+    $(this).closest('.page_product').toggleClass('full');
+  });
+});
