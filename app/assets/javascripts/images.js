66b0aada0917b709cdda467932a76cf9f9e55f65
Andy Braga <ab@luckypike.com>
2018-03-07 17:00:52 +0300

    Лишний код

--- app/assets/javascripts/images.js
+++ app/assets/javascripts/images.js
@@ -1,8 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(function(){
-  $('.product_images').on('click', function() {
-    console.log('QQQ');
+  $('.product_images .images_list').on('click', function() {
     $(this).closest('.page_product').toggleClass('full');
   });
 });
