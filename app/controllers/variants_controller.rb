73804268f19a1a890c1b4cc65e98eef2d928af57
Sidlike <almostkolyan@gmail.com>
2018-05-08 14:11:52 +0300

    Поправлен косяк с допустимыми параметрами

--- app/controllers/variants_controller.rb
+++ app/controllers/variants_controller.rb
@@ -92,6 +92,6 @@ class VariantsController < ApplicationController
   end
 
   def variant_params
-    params.require(:variant).permit(:product_id, :color_id, :out_of_stock, :state, sizes: [], :created_at)
+    params.require(:variant).permit(:product_id, :color_id, :out_of_stock, :state, :created_at, sizes: [])
   end
 end
