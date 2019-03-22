15e6ee398dc35fcbc657c76439a8d3e132b6490c
Andy Braga <bragovo@gmail.com>
2019-03-23 00:10:06 +0300

    Слайдер на главной и правки иконок

--- app/javascript/components/Header.js
+++ app/javascript/components/Header.js
@@ -4,12 +4,10 @@ import classNames from 'classnames'
 import styles from './Header.module.css'
 
 import Logo from '!svg-react-loader?!../images/golovina.svg'
-import Wishlist from '!svg-react-loader?!../images/icons/wishlist.svg'
-import Cart from '!svg-react-loader?!../images/icons/cart.svg'
 
 class Header extends Component {
   render () {
-    const { white } = this.props
+    const { white, cart } = this.props
 
     return (
       <header className={classNames(styles.root, { [styles.white]: white })}>
@@ -26,11 +24,26 @@ class Header extends Component {
 
         <div className={styles.caw}>
           <a href="#" className={styles.wishlist}>
-            <Wishlist />
+            <svg viewBox="0 0 24 24">
+              <path className="a" d="M9.09,5.51A4,4,0,0,0,6.18,6.72,4.22,4.22,0,0,0,6,12.38c0,.07,4.83,4.95,6,6.12,2.38-2.42,5.74-5.84,6-6.12v0a4,4,0,0,0,1-2.71,4.13,4.13,0,0,0-1.19-2.92,4.06,4.06,0,0,0-5.57-.21L12,6.72l-.25-.21A4.05,4.05,0,0,0,9.09,5.51Z"/>
+            </svg>
           </a>
 
-          <a href="#" className={styles.cart}>
-            <Cart />
+          <a href="#" className={classNames(styles.cart, { [styles.active]: cart > 0 })}>
+            <svg viewBox="0 0 48 48">
+              <g>
+                <path className="a" d="M29.18,21.76H18.82l-.82,9H30Z" />
+                <path className="b" d="M21,20.25a3,3,0,0,1,6,0" />
+              </g>
+
+              <g className={classNames(styles.counter, { [styles.active]: cart > 0 })}>
+                <circle className="c" cx="32" cy="16" r="8" />
+                <text className="d" x="32" y="19">
+                  {cart}
+                </text>
+
+              </g>
+            </svg>
           </a>
         </div>
       </header>
--- app/javascript/components/Index.js
+++ app/javascript/components/Index.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react'
-import Glide, { Controls, Breakpoints } from '@glidejs/glide/dist/glide.modular.esm'
+import Glide from '@glidejs/glide'
+import classNames from 'classnames'
+
+import '@glidejs/glide/dist/css/glide.core'
+import styles from './Index.module.css'
 
 class Index extends Component {
+  mount = React.createRef()
+
+  componentDidMount() {
+    this.glide = new Glide(this.mount.current, {
+      type: 'carousel',
+    })
+    this.glide.mount()
+  }
+
   render () {
+    const { slides } = this.props
+
     return (
       <>
-        <div>
-          Index
+        <div className="glide" ref={this.mount}>
+          <div className="glide__track" data-glide-el="track">
+            <div className={classNames('glide__slides', styles.slides)}>
+              {slides.map((slide, _) =>
+                <div key={_} className={classNames('glide__slide', styles.slide)}>
+                  <div className={styles.text}>
+                    <div className={styles.title}>
+                      {slide.name}
+                    </div>
+
+                    <div className={styles.link}>
+                      {slide.link_name}
+                    </div>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
         </div>
       </>
     )
