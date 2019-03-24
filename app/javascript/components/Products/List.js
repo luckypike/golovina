31ee291e867a2c1ef2f3f6bb179b448f13615139
Andy Braga <bragovo@gmail.com>
2019-03-24 12:07:08 +0300

    Оформление списка товаров

--- app/javascript/components/Products/List.js
+++ app/javascript/components/Products/List.js
@@ -10,15 +10,21 @@ class List extends Component {
     return (
       <div className={styles.root}>
         {variants.filter(variant => variant.image).map((variant, _) =>
-          <div key={variant.id} className={classNames(styles.item, styles[`i${_ % 6}`])}>
+          <a href="#" key={variant.id} className={classNames(styles.item, styles[`i${_ % 6}`])}>
             <div className={styles.image}>
               <img src={variant.image} />
             </div>
 
-            <div className={styles.title}>
-              {variant.title}
+            <div className={styles.desc}>
+              <div className={styles.title}>
+                {variant.title}
+              </div>
+
+              <div className={styles.price}>
+                {variant.price} ₽
+              </div>
             </div>
-          </div>
+          </a>
         )}
       </div>
     )
