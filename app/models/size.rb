b4124c0fd9408eee5c17e0fcb380c4f9c2ea819a
Andy Braga <bragovo@gmail.com>
2019-04-06 08:24:17 +0300

    Исправлены миграции наличия товаров

--- app/models/size.rb
+++ app/models/size.rb
@@ -1,4 +1,6 @@
 class Size < ApplicationRecord
   belongs_to :sizes_group
   has_many :availabilities, dependent: :destroy
+  has_many :carts, dependent: :destroy
+  has_many :order_items, dependent: :nullify
 end
--- db/migrate/20190401135442_create_availabilities.rb
+++ db/migrate/20190401135442_create_availabilities.rb
@@ -3,16 +3,18 @@ class CreateAvailabilities < ActiveRecord::Migration[5.2]
     create_table :availabilities do |t|
       t.references :variant, foreign_key: true
       t.references :size, foreign_key: true
-      t.integer :count
+      t.references :store, foreign_key: true
+      t.integer :quantity
 
       t.timestamps
     end
 
     reversible do |dir|
       dir.up do
+        store_id = Store.first.id
         Variant.find_each do |variant|
-          JSON.parse(variant.sizes_before_type_cast).each do |size, count|
-            variant.availabilities.create(count: count, size_id: size.to_i)
+          JSON.parse(variant.sizes_before_type_cast).each do |size, quantity|
+            variant.availabilities.create(quantity: quantity, size_id: size.to_i, store_id: store_id) if quantity.to_i > 0
           end
         end
       end
