57b4304eda0bc8f459d7eb28be8b75a9eebdd284
Andy Braga <bragovo@gmail.com>
2019-08-27 09:44:55 +0300

    Хинт при заказе

--- app/models/order.rb
+++ app/models/order.rb
@@ -40,7 +40,7 @@ class Order < ApplicationRecord
   has_many :order_items, dependent: :destroy
   accepts_nested_attributes_for :order_items
 
-  validates :address, presence: true
+  # validates :address, presence: true
   validate :quantity_cannot_be_greater_than_total, on: :create
 
   include ActionView::Helpers::NumberHelper
