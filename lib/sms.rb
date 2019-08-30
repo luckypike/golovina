a54c321c30044d190cc0836dd3df3e158c7506ec
Andy Braga <bragovo@gmail.com>
2019-08-30 09:54:11 +0200

    Замена гейта и отправителя

--- lib/sms.rb
+++ lib/sms.rb
@@ -3,6 +3,6 @@ require 'addressable/uri'
 
 class Sms
   def self.message phone, message
-    Net::HTTP.get(Addressable::URI.parse("https://gate.smsaero.ru/send/?user=#{Rails.application.credentials[:sms][:login]}&password=#{Rails.application.credentials[:sms][:password]}&to=#{phone}&text=#{message}&from=mint-store"))
+    Net::HTTP.get(Addressable::URI.parse("https://gate.smsaero.ru/send/?user=#{Rails.application.credentials[:sms][:login]}&password=#{Rails.application.credentials[:sms][:password]}&to=#{phone}&text=#{message}&from=Golovina"))
   end
 end
