077ae35998aa037b5a6428adb81b32995da93d04
Andy Braga <ab@luckypike.com>
2019-10-10 14:24:04 +0300

    Убрана замена первой цифры телефона

--- app/models/user.rb
+++ app/models/user.rb
@@ -74,7 +74,7 @@ class User < ApplicationRecord
   class << self
     def prepare_phone text
       text ||= ''
-      text = text.sub(/^8/, '7').gsub(/[\D]/, '')
+      text = text.gsub(/[\D]/, '')
       text.size == 11 ? text : false
     end
   end
