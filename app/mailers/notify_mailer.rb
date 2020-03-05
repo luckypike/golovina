7d3e2e911c28b6ce370231d4e23f3ccc45b0dfc6
Sidlike <almostkolyan@gmail.com>
2020-03-05 16:42:33 +0300

    Добавлены переводы

--- app/mailers/notify_mailer.rb
+++ app/mailers/notify_mailer.rb
@@ -1,6 +1,6 @@
 class NotifyMailer < ApplicationMailer
   def notify_mailer(mail, variant)
     @variant = variant
-    mail(to: mail, subject: 'Уведомление о поступлении товара')
+    mail(to: mail, subject: I18n.t('mailers.subjects.notify'))
   end
 end
