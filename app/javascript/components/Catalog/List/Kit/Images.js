16527a43ae7501d1287c89e883adef4400cb69c5
Andy Braga <ab@luckypike.com>
2020-09-01 18:09:31 +0300

    Добавлены стрелки переключения

--- app/javascript/components/Catalog/List/Kit/Images.js
+++ app/javascript/components/Catalog/List/Kit/Images.js
@@ -17,8 +17,9 @@ Images.propTypes = {
 }
 
 export default function Images ({ images, video, poster, videoHide }) {
-  const [sliderRef] = useKeenSlider({
+  const [sliderRef, slider] = useKeenSlider({
     initial: 0,
+    // controls: false,
     slideChanged (s) {
       setCurrent(s.details().relativeSlide + 1)
     }
@@ -37,37 +38,59 @@ export default function Images ({ images, video, poster, videoHide }) {
   }
 
   return (
-    <div className={classNames('keen-slider', styles.slider)} ref={sliderRef}>
-      <div className={classNames(styles.nav, { [styles.isSlide]: isSlides() > 1 })}>
-        {current} / { isSlides() }
+    <>
+      <div className={classNames('keen-slider', styles.slider)} ref={sliderRef}>
+        <div className={classNames(styles.nav, { [styles.isSlide]: isSlides() > 1 })}>
+          {current} / { isSlides() }
+        </div>
+
+        {video && !videoHide &&
+          <div className={classNames('keen-slider__slide', styles.image)}>
+            <Video
+              poster={poster && `https://storage.yandexcloud.net/golovina-production/${poster}`}
+              src={`https://golovina.store/video/${video}.mp4`}
+            />
+          </div>
+        }
+
+        {images && images.map((image, index) =>
+          <React.Fragment key={image.id}>
+            <div className={classNames('keen-slider__slide', styles.image)}>
+              <img src={image.thumb} />
+            </div>
+
+            {index === 0 && video && videoHide &&
+              <div className={classNames('keen-slider__slide', styles.image)}>
+                <Video
+                  poster={poster && `https://storage.yandexcloud.net/golovina-production/${poster}`}
+                  src={`https://golovina.store/video/${video}.mp4`}
+                />
+              </div>
+            }
+          </React.Fragment>
+        )}
       </div>
 
-      {video && !videoHide &&
-        <div className={classNames('keen-slider__slide', styles.image)}>
-          <Video
-            poster={poster && `https://storage.yandexcloud.net/golovina-production/${poster}`}
-            src={`https://golovina.store/video/${video}.mp4`}
-          />
+      <div className={styles.pn} onClick={e => e.preventDefault()}>
+        <div
+          onClick={e => e.preventDefault() || slider.prev()}
+          className={classNames(styles.prev, { [styles.inactive]: slider && current === 1 })}
+        >
+          <svg viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M18 22L10 14L18 6" stroke="#fafafa" strokeWidth="1.5" />
+          </svg>
         </div>
-      }
 
-      {images && images.map((image, index) =>
-        <React.Fragment key={image.id}>
-          <div className={classNames('keen-slider__slide', styles.image)}>
-            <img src={image.thumb} />
-          </div>
-
-          {index === 0 && video && videoHide &&
-            <div className={classNames('keen-slider__slide', styles.image)}>
-              <Video
-                poster={poster && `https://storage.yandexcloud.net/golovina-production/${poster}`}
-                src={`https://golovina.store/video/${video}.mp4`}
-              />
-            </div>
-          }
-        </React.Fragment>
-      )}
-    </div>
+        <div
+          onClick={e => e.preventDefault() || slider.next()}
+          className={classNames(styles.next, { [styles.inactive]: slider && current === slider.details().size })}
+        >
+          <svg viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10 22L18 14L10 6" stroke="#fafafa" strokeWidth="1.5" />
+          </svg>
+        </div>
+      </div>
+    </>
   )
 }
 
