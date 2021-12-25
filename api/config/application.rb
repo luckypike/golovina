73fa8455b62543d18c0b335a62bafdb88d2c2d09
Andy Braga <bragovo@gmail.com>
2021-12-25 16:53:03 +0300

    Add sidekiq

--- api/config/application.rb
+++ api/config/application.rb
@@ -24,7 +24,7 @@ module Golovina
 
     config.time_zone = 'Europe/Moscow'
 
-    config.active_job.queue_adapter = :delayed_job
+    config.active_job.queue_adapter = :sidekiq
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
