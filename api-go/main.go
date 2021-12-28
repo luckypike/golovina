f0a9889d204b6afba8f836d20b8c19628c2394e6
Andy Braga <bragovo@gmail.com>
2021-12-28 20:18:27 +0300

    Decode rails session

--- api-go/main.go
+++ api-go/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"golovina/utils"
 	"net/http"
+	"net/url"
 
 	"github.com/labstack/echo/v4"
 )
@@ -9,6 +12,12 @@ import (
 func main() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
+		cookie, _ := c.Cookie("_golovina_session")
+
+		encryptedCookie, _ := url.QueryUnescape(cookie.Value)
+		userId := utils.Decrypt(encryptedCookie)
+		fmt.Println(userId)
+
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 	e.Logger.Fatal(e.Start(":3000"))
---
+++ api-go/utils/sessions.go
@@ -0,0 +1,37 @@
+package utils
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/sha1"
+	"encoding/base64"
+	"encoding/json"
+	"os"
+	"strings"
+
+	"golang.org/x/crypto/pbkdf2"
+)
+
+func Decrypt(cookie string) float64 {
+	secret := pbkdf2.Key([]byte(os.Getenv("RAILS_SECRET_KEY")), []byte(os.Getenv("RAILS_SALT")), 1000, 32, sha1.New)
+
+	block, _ := aes.NewCipher(secret)
+	aesgcm, _ := cipher.NewGCM(block)
+
+	encryptedData := strings.Split(cookie, "--")
+
+	data, _ := base64.StdEncoding.DecodeString(encryptedData[0])
+	iv, _ := base64.StdEncoding.DecodeString(encryptedData[1])
+	authTag, _ := base64.StdEncoding.DecodeString(encryptedData[2])
+
+	data = []byte(append(data, authTag...))
+	plaintext, _ := aesgcm.Open(nil, iv, data, nil)
+
+	var mp map[string]interface{}
+
+	json.Unmarshal(plaintext, &mp)
+	b, _ := base64.StdEncoding.DecodeString(mp["_rails"].(map[string]interface{})["message"].(string))
+	json.Unmarshal(b, &mp)
+
+	return mp["warden.user.user.key"].([]interface{})[0].([]interface{})[0].(float64)
+}
