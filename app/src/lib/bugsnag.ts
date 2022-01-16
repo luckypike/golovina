6a7a485e44e48050d2dde78b5fb0b0bbfa90ffdc
Andy Braga <bragovo@gmail.com>
2022-01-16 22:01:31 +0300

    Add bugsnag

---
+++ app/src/lib/bugsnag.ts
@@ -0,0 +1,9 @@
+import Bugsnag from '@bugsnag/js'
+import BugsnagPluginReact from '@bugsnag/plugin-react'
+
+Bugsnag.start({
+  apiKey: process.env.NEXT_PUBLIC_BUGSNAG_API_KEY ?? '',
+  plugins: [new BugsnagPluginReact()]
+})
+
+export default Bugsnag
--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,13 +1,18 @@
 import App, { AppContext, AppProps } from 'next/app'
-import { useEffect, useState } from 'react'
+import React, { FC, useEffect, useState } from 'react'
 import axios, { AxiosRequestConfig } from 'axios'
+import { BugsnagPluginReactResult } from '@bugsnag/plugin-react'
 
 import { Header } from '../layout/Header'
 import { RootContext } from '../services/useRootContext'
 import { RootStore, SessionData } from '../services/stores/RootStore'
+import Bugsnag from '../lib/bugsnag'
 
 import 'normalize.css'
 import '../css/app.css'
+
+const plugin = Bugsnag.getPlugin('react') as BugsnagPluginReactResult
+const ErrorBoundary = plugin.createErrorBoundary(React)
 
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL
 
@@ -15,13 +20,19 @@ function AppPage({ Component, pageProps, sessionData }: AppProps & { sessionData
   const [rootStore] = useState(new RootStore(sessionData))
 
   return (
+    <ErrorBoundary FallbackComponent={ErrorView}>
     <RootContext.Provider value={rootStore}>
       <Header />
       <main className="main">
         <Component {...pageProps} />
       </main>
     </RootContext.Provider>
+    </ErrorBoundary>
   )
+}
+
+const ErrorView: FC = () => {
+  return (<div>ERROR PAGE</div>)
 }
 
 AppPage.getInitialProps = async (appContext: AppContext) => {
