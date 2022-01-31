26fd8ce6322f890b64fc4940f885163c523e2800
Andy Braga <bragovo@gmail.com>
2022-01-31 09:02:18 +0300

    From name

--- api/app/mailers/application_mailer.rb
+++ api/app/mailers/application_mailer.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 class ApplicationMailer < ActionMailer::Base
-  default from: "Golovina <#{Rails.application.credentials[Rails.env.to_sym][:mail][:username]}>"
+  default from: "Golovina Mari <#{Rails.application.credentials[Rails.env.to_sym][:mail][:username]}>"
   layout 'mailer'
 end
