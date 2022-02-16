f759ad60df9769c349ebc239281d4bb17149412d
Andy Braga <bragovo@gmail.com>
2022-02-16 19:02:44 +0300

    Send user to unisend

--- api/app/commands/subscriptions/process_cmd.rb
+++ api/app/commands/subscriptions/process_cmd.rb
@@ -5,6 +5,12 @@ module Subscriptions
     input :subscription
 
     def call
+      UnisenderClient.subscribe(
+        fields: { email: subscription.email, Name: [subscription.first_name, subscription.last_name].join(' ') },
+        double_optin: 3,
+        list_ids: [Figaro.env.send("unisender_list_#{subscription.locale}!")]
+      )
+
       subscription.update(state: :active)
     end
   end
