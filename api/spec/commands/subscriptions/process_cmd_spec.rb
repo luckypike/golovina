4ee2243e304c4dec693651c56fd71a31be83a334
Andy Braga <bragovo@gmail.com>
2022-03-10 23:31:08 +0100

    Add redis, vips and mock client

--- api/spec/commands/subscriptions/process_cmd_spec.rb
+++ api/spec/commands/subscriptions/process_cmd_spec.rb
@@ -8,12 +8,15 @@ RSpec.describe Subscriptions::ProcessCmd, :aggregate_failures do
     before do
       stub_request(:get, "https://api.unisender.com/ru/api/subscribe")
         .with(query: hash_including({}))
+
+      allow(UnisenderClient).to receive(:subscribe).and_return(true)
     end
 
     it do
       subject
 
       expect(subscription.reload.active?).to be_truthy
+      expect(UnisenderClient).to have_received(:subscribe)
     end
   end
 end
