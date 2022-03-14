9ddd51870c84a221e2930185fe888959d86753d5
Andy Braga <bragovo@gmail.com>
2022-03-14 10:09:32 +0100

    Revert not_found

--- api/app/controllers/concerns/application_exception_handler.rb
+++ api/app/controllers/concerns/application_exception_handler.rb
@@ -16,8 +16,8 @@ module ApplicationExceptionHandler
       head :bad_request
     end
 
-    # rescue_from ActiveRecord::RecordNotFound do
-    #   head :not_found
-    # end
+    rescue_from ActiveRecord::RecordNotFound do
+      head :not_found
+    end
   end
 end
