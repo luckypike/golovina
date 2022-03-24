cbecabeb075d29724f0b7fa4fba51a0fb80c06a2
Andy Braga <bragovo@gmail.com>
2022-03-24 16:52:26 +0100

    Remove inactive items from cart (#84)

--- api/spec/commands/carts/show_cmd_spec.rb
+++ api/spec/commands/carts/show_cmd_spec.rb
@@ -4,11 +4,24 @@ RSpec.describe Carts::ShowCmd, :aggregate_failures do
   subject(:cmd) { described_class.call(user: user) }
 
   let(:user) { create(:api_user) }
+  let(:order) { create(:api_order, :cart, user: user) }
 
   describe "#call" do
-    # TODO: Add some test
-    xit do
-      cmd
+    context "when cart containt inactive variant" do
+      let(:variant_unpub) { create(:variant, state: :unpub) }
+      let(:variant_archived) { create(:variant, state: :archived) }
+
+      before do
+        create(:api_order_item, order: order)
+        create(:api_order_item, order: order, variant: variant_unpub)
+        create(:api_order_item, order: order, variant: variant_archived)
+      end
+
+      it { is_expected.to be_success }
+
+      it do
+        expect { cmd }.to change { Api::OrderItem.where(order: order).all.size }.by(-2)
+      end
     end
   end
 end
