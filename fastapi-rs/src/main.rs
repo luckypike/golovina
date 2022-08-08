9a8c715b30e50ef95499b0ee61601ab108d26534
Andy Braga <bragovo@gmail.com>
2022-08-08 22:27:55 +0300

    Add sentry

--- fastapi-rs/src/main.rs
+++ fastapi-rs/src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 #[macro_use]
 extern crate diesel;
 extern crate dotenv;
@@ -8,5 +10,15 @@ pub mod schema;
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
+
+    let _sentry = sentry::init((
+        env::var("SENTRY_URL").unwrap(),
+        sentry::ClientOptions {
+            release: sentry::release_name!(),
+            traces_sample_rate: 1.0,
+        ..Default::default()
+        }
+    ));
+
     app::run().await
 }
