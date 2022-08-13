a8055e5e3029d6fbfd0df17dbbec51e0243e80d4
Andy Braga <bragovo@gmail.com>
2022-08-13 09:33:13 +0300

    Only editors can use upload endpoint

--- fastapi-rs/src/app/uploads/mod.rs
+++ fastapi-rs/src/app/uploads/mod.rs
@@ -1,21 +1,31 @@
 use aws_sdk_s3::Client;
 use axum::{
   extract::Multipart,
-  Extension, Json
+  Extension, Json, http::StatusCode
 };
 
 use self::data::UploadData;
+
+use super::User;
 mod service;
 mod data;
 
-pub async fn create(mut multipart: Multipart, Extension(s3_client): Extension<Client>) -> Json<UploadData> {
+pub async fn create(
+  user: User, mut multipart: Multipart,
+  Extension(s3_client): Extension<Client>
+) -> Result<Json<UploadData>, StatusCode> {
+
+  if !user.editor {
+    return Err(StatusCode::UNAUTHORIZED);
+  }
+
   let key = if let Some(file) = multipart.next_field().await.unwrap() {
     service::create(s3_client, file.bytes().await.unwrap()).await
   } else {
-    panic!("File cannot be upload!");
+    return Err(StatusCode::INTERNAL_SERVER_ERROR);
   };
 
-  Json(UploadData {
+  Ok(Json(UploadData {
     key
-  })
+  }))
 }
